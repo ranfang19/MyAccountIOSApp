43bc22cc6e34cade2d51bb6a19dececd69e1aa4d
ranfang19 <ranfang19@gmail.com>
2019-12-05 15:15:58 +0100

    Initial Commit

---
+++ MyAccountappIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAccountappIOS
+//
+//  Created by Ran FANG on 2019/12/5.
+//  Copyright © 2019 Ran FANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
