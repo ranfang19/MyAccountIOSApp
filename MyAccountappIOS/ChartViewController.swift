6a52c402f660424836c828aecfc7384dba1ff59f
ranfang19 <ranfang19@gmail.com>
2019-12-18 10:56:47 +0100

    chart view add date picker

--- MyAccountappIOS/ChartViewController.swift
+++ MyAccountappIOS/ChartViewController.swift
@@ -11,7 +11,9 @@ import Charts
 import CoreData
 
 
-class ChartViewController: UIViewController {
+class ChartViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+
+    
     
     var isExpense:Bool = false
     @IBAction func incomeExpense(_ sender: UISegmentedControl) {
@@ -23,14 +25,74 @@ class ChartViewController: UIViewController {
         }
     }
     
-    @IBAction func leftButton(_ sender: UIButton) {
+    @IBAction func pickerButton(_ sender: UIButton) {
     }
     
-    @IBAction func rightButton(_ sender: UIButton) {
-    }
     
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    var yearArray:[String] = [String]()
+    var monthArray:[String] = [String]()
+    
+    var yearData:String = "2019"
+    var monthData:String = "Dec"
+    
+    @IBAction func triangleButton(_ sender: UIButton) {
+        self.picker?.dataSource = self
+        self.picker?.delegate = self
+        
+        yearArray = ["2017","2018","2019","2020","2021","2022"]
+        monthArray = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"]
+
+        pickerViewHiddenStatus(pvStatus: false)
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        pickerViewHiddenStatus(pvStatus: true)
+    }
+    
+    @IBAction func doneButton(_ sender: Any) {
+        pickerViewHiddenStatus(pvStatus: true)
+        yearLabel.text = yearData
+        monthLabel.text = monthData
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return yearArray.count
+        }
+        
+        return monthArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return yearArray[row]
+        }
+        return monthArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent  component: Int) {
+        if component == 0 {
+            yearData = yearArray[row]
+        }
+        monthData = monthArray[row]
+    }
+    
+    func pickerViewHiddenStatus(pvStatus:Bool) {
+        picker.isHidden = pvStatus
+        cancelButton.isHidden = pvStatus
+        doneButton.isHidden = pvStatus
+    }
+    
     
     
     
@@ -46,6 +108,11 @@ class ChartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        yearLabel.text = "2019"
+        monthLabel.text = "Dec"
+        
+        pickerViewHiddenStatus(pvStatus: true)
 
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
